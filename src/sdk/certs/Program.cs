b1bc057b8d9a8137c2753ea53fdafbc3e70dc2b3
Martin Loitzl <martin@loitzl.com>
2025-03-29 13:17:56 +0100

    Add certificate listing tool with X509Store implementation

---
+++ src/sdk/certs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+X509Store store = new X509Store("My", StoreLocation.CurrentUser);
+
+store.Open(OpenFlags.ReadOnly);
+X509Certificate2Collection col = store.Certificates;
+
+foreach (X509Certificate2 cert in col)
+{
+    Console.WriteLine($"Subject: {cert.Subject}");
+    Console.WriteLine($"Issuer: {cert.Issuer}");
+    Console.WriteLine($"Valid From: {cert.NotBefore}");
+    Console.WriteLine($"Valid To: {cert.NotAfter}");
+    Console.WriteLine($"Thumbprint: {cert.Thumbprint}");
+    Console.WriteLine();
+}
